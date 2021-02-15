bda78ea50cb4113dbd8e8b5f47d6514fb08e2511
Keven <keven.leandro.95@gmail.com>
2021-02-15 03:06:00 -0300

    distancia Entre pontos

---
+++ DistanciaDoisPontos.py
@@ -0,0 +1,12 @@
+# lib com  raiz quadrada 
+import math
+
+linha1 = input().split(" ")
+linha2 = input().split(" ")
+
+x1,y1 = linha1
+x2,y2 = linha2
+
+distancia_entre_pontos = math.sqrt(((float(x2) - float(x1))*(float(x2) - float(x1))) + ((float(y2)-float(y1)) *(float(y2)-float(y1))))
+
+print("%0.4f" %distancia_entre_pontos)
